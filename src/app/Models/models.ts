0f6a382da8d4ec55b85251d00b6a29d1467941c9
Ercument <ercukalkann@gmail.con>
2022-05-08 17:26:18 +0300

    base Component and Service were created and entity models were added

---
+++ src/app/Models/models.ts
@@ -0,0 +1,33 @@
+export class Entity {
+    id!: number;
+    description!: string;
+    amount!: number;
+
+    constructor() {}
+}
+
+export class FrequentEntity extends Entity {
+    frequency!: string;
+
+    constructor() { super(); }
+}
+
+export class OneTimeIncome extends Entity {
+
+    constructor() { super(); }
+}
+
+export class OneTimeExpense extends Entity {
+
+    constructor() { super(); }
+}
+
+export class RegularIncome extends FrequentEntity {
+
+    constructor() { super(); }
+}
+
+export class RegularExpense extends FrequentEntity {
+
+    constructor() { super(); }
+}
